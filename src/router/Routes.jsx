9699dd853b5bd010d1b8d0645a6bad22e8306172
Taegyeong Park <tgyeongt@gmail.com>
2024-12-27 23:08:20 +0900

    Feat: Navbar 설정정

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,9 +1,8 @@
 import { Routes as ReactRouters, Route } from 'react-router-dom';
 
 import HomePage from '@pages/HomePage';
-import LoginPage from '@pages/LoginPage';
-import SignupPage from '@pages/SignupPage';
-import MyPage from '@pages/MyPage';
+import SearchPage from '@pages/SearchPage';
+import LibraryPage from '@pages/LibraryPage';
 import MainLayout from '@layouts/MainLayout';
 
 export default function Routes() {
@@ -12,9 +11,8 @@ export default function Routes() {
       {/* 레이아웃 공통 적용 */}
       <Route path="/" element={<MainLayout />}>
         <Route index={true} element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="signup" element={<SignupPage />} />
-        <Route path="mypage" element={<MyPage />} />
+        <Route path="search" element={<SearchPage />} />
+        <Route path="library" element={<LibraryPage />} />
       </Route>
     </ReactRouters>
   );
